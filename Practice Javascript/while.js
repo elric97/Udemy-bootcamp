ee754a29e0ac3d5967d66f3d0dd9d5bb8cc92128
elric97 <sharma.rachit882@gmail.com>
2017-06-24 22:48:20 +0530

    added for for loop also

--- Practice Javascript/while.js
+++ Practice Javascript/while.js
@@ -1,3 +1,4 @@
+console.log("Using While Loop");
 var num = -10;
 while(num<=19)
 {
@@ -27,4 +28,24 @@ while (num4<=50)
 	if (num4%5===0 && num4%3===0)
 		console.log(num4);
 	num4++;
+}
+console.log("Using For Loop");
+for (var x=-10;x<=19;x++)
+{
+	console.log(x);
+}
+for (var x=10;x<=40;x++)
+{
+	if (x%2===0)
+		console.log(x);
+}
+for (var x=300;x<=333;x++)
+{
+	if (x%2!==0)
+		console.log(x);
+}
+for (var x=5;x<=50;x++)
+{
+	if (x%5===0 && x%3===0)
+		console.log(x);
 }
