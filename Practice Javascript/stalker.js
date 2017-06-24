2b842cee9514f8476dfb076aed3054955ca59972
elric97 <sharma.rachit882@gmail.com>
2017-06-24 13:04:09 +0530

    stalker exercise

---
+++ Practice Javascript/stalker.js
@@ -0,0 +1,5 @@
+var name=prompt("enter your first name");
+var last=prompt("enter your last name");
+var age=prompt("enter your age");
+console.log("Your full name is "+name+" "+last);
+console.log("You are "+age+" years old");
