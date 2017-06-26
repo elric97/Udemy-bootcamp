fdb9c321ebe564e2b741ebbe53dabbcf5c5b33d8
elric97 <sharma.rachit882@gmail.com>
2017-06-26 14:16:23 +0530

    final

--- Practice Javascript/todo.js
+++ Practice Javascript/todo.js
@@ -5,11 +5,23 @@ while (true)
 	if (ans==="new")
 	{
 		var item=prompt("enter the task");
-		todo.push("item");
+		todo.push(item);
 	}
 	else if (ans==="list")
-		console.log(todo);
-	else
+	{
+		console.log("**************")
+		todo.forEach(function(val,index)
+		{
+			console.log(index + ": " + val);
+		})
+		console.log("**************");
+	}
+	else if (ans==="delete")
+	{
+		var ind=prompt("enter the index to be deleted");
+		todo.splice(ind,1);
+	}
+	else if(ans==="quit")
 		break;
 
 	var ans=prompt("enter your choice").toLowerCase();
