041aa7e584760989758da88a5c7a5cb3f66be122
elric97 <sharma.rachit882@gmail.com>
2017-06-30 14:28:07 +0530

    basic

---
+++ Jquery practice/try.js
@@ -0,0 +1,8 @@
+//purple background
+$("div").css("backgroundColor","purple");
+
+$(".highlight").css("width","200px");
+
+$("#third").css("border","2px solid orange");
+
+$("div:nth-of-type(1)").css("color","pink");
