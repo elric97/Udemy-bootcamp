080e8a95ec5e7dcc96a3baff7b4a262196458513
elric97 <sharma.rachit882@gmail.com>
2017-07-05 23:41:31 +0530

    added "/bye"

--- Node Practice/ExpressApp/app.js
+++ Node Practice/ExpressApp/app.js
@@ -6,7 +6,11 @@ app.get("/", function(req,res)
 {
 	res.send("Hi there!");
 });
-
+//bye
+app.get("/bye",function(req,res)
+{
+	res.send("GoodBye");
+});
 //tell to listen for request
 app.listen(3000, function()
 {
