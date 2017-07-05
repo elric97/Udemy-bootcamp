3f74c787acb84a87aaec682e96af9c6cc4fff125
elric97 <sharma.rachit882@gmail.com>
2017-07-06 01:18:13 +0530

    pattern

--- Node Practice/ExpressApp/app.js
+++ Node Practice/ExpressApp/app.js
@@ -11,6 +11,11 @@ app.get("/bye",function(req,res)
 {
 	res.send("GoodBye");
 });
+//defining a pattern to match 
+app.get("/r/:anyName",function(req,res)
+{
+	res.send("hello there");
+});
 //for any other get request other than defined we use * but we have to keep this below all the other routes to avoid matching to this
 app.get("*",function(req,res)
 {
