b4ad1eaa5007eecbb4f41a3422167d536f374fd8
elric97 <sharma.rachit882@gmail.com>
2017-07-06 01:08:19 +0530

    modif

--- Node Practice/ExpressApp/app.js
+++ Node Practice/ExpressApp/app.js
@@ -11,6 +11,11 @@ app.get("/bye",function(req,res)
 {
 	res.send("GoodBye");
 });
+//for any other get request other than defined we use * but we have to keep this below all the other routes to avoid matching to this
+app.get("*",function(req,res)
+{
+	res.send("404 error not found");
+});
 //tell to listen for request
 app.listen(3000, function()
 {
