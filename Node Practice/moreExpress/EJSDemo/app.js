f7f6d0db0ede3b71deb8a48d0f9c5bd3ae08aede
elric97 <sharma.rachit882@gmail.com>
2017-07-06 19:24:41 +0530

    more edit

--- Node Practice/moreExpress/EJSDemo/app.js
+++ Node Practice/moreExpress/EJSDemo/app.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var app = express();
+
+app.use(express.static("public")); //to use the files present in public directories
+
+app.set("view engine","ejs");
+
 //make a views directory for the ejs files as express looks in that
 app.get("/",function(req,res)
 {
-   res.render("home.ejs");
+   res.render("home");
 });
 app.get("/fallinlove/:name",function(req,res)
 {
     var thing=req.params.name;
-    res.render("love.ejs",{thingVar: thing});
+    res.render("love",{thingVar: thing});
 });
 
 app.get("/post",function(req, res) 
@@ -20,7 +25,7 @@ app.get("/post",function(req, res)
         {title: "yoho",author:"yoho 3o" }
     ];
     
-    res.render("posts.ejs",{posts: post});
+    res.render("posts",{posts: post});
 });
 app.listen(3000,function()
 {
