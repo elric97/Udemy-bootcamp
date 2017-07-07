1d67a5aa0005c909e27abf47f908d6bace4bb3a4
elric97 <sharma.rachit882@gmail.com>
2017-07-08 01:42:36 +0530

    movie search

---
+++ Node Practice/APIs/movieSearch/app.js
@@ -0,0 +1,29 @@
+var express=require("express");
+var app=express();
+var request=require("request");
+
+app.set("view engine","ejs");
+
+app.get("/",function(req, res) 
+{
+   res.render("search"); 
+});
+
+app.get("/result",function(req,res)
+{
+    var val=req.query.name;
+    var url="http://www.omdbapi.com/?s=" + val +"&apikey=thewdb";
+    request(url,function(error,response,body)
+    {
+        if(!error && response.statusCode==200)
+        {
+            var data=JSON.parse(body);
+            res.render("results",{data: data});
+        }
+    }); 
+});
+
+app.listen(process.env.PORT,process.env.IP,function()
+{
+   console.log("Movie app has started"); 
+});
