fb71349e5246c0f0e233b1d7a37b69e898f9f9be
elric97 <sharma.rachit882@gmail.com>
2017-07-11 23:19:42 +0530

    first

---
+++ Node Practice/YelpCamp/v3/app.js
@@ -0,0 +1,89 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds"),
+    mongoose = require("mongoose");
+
+seedDB();
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+
+app.use(bodyParser.urlencoded({extended: true})); // to set body parser to get data 
+app.set("view engine","ejs");
+
+app.get("/",function(req,res)
+{
+    res.render("landing");
+});
+
+app.get("/campgrounds",function(req,res)
+{
+    //get all campgrounds from db
+    Campground.find({},function(err,allCampgrounds)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("index",{data: allCampgrounds});
+        }
+    });
+    // res.render("campgrounds",{data: campgrounds});
+});
+
+//making a new campground
+app.post("/campgrounds", function(req,res)
+{
+   //get data from from and add to campfround array
+   var name=req.body.name;
+   var image=req.body.image;
+   var dsc=req.body.description;
+   var val={name: name,image: image,description: dsc};
+   //create a new campground and save to a database 
+   Campground.create(val,function(err,nval)
+   {
+       if(err)
+       {
+           console.log(err);
+       }
+       else
+       {
+           //redirect back to campground
+           res.redirect("/campgrounds"); //default is a get request when redirecting so get method will run 
+       }
+   });
+});
+
+//a form to add new forms 
+app.get("/campgrounds/new",function(req, res) 
+{
+    res.render("new");
+});
+
+//order is important
+//shows more info about that campground
+app.get("/campgrounds/:id",function(req, res) 
+{
+    //find the campground with id
+    var id=req.params.id;
+    //we use the populate function for this 
+    Campground.findById(req.params.id).populate("comments").exec(function(err,val)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("show",{val: val});
+        }
+    });
+    //show template
+});
+
+app.listen(process.env.PORT,process.env.IP,function()
+{
+   console.log("Yelp Camp server started"); 
+});
---
+++ Node Practice/YelpCamp/v3/models/campground.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+//schema set up
+var campgroundSchema = new mongoose.Schema(
+    {
+       name: String,
+       image: String,
+       description: String,
+       comments:[
+           {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: "Comment" //name of model
+           }]
+    });
+    
+module.exports = mongoose.model("Campground",campgroundSchema);
