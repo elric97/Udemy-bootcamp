db6be5942fab8b648b932fad356298bd861d0c58
elric97 <sharma.rachit882@gmail.com>
2017-08-04 00:00:21 +0530

    all files

---
+++ Node Practice/YelpCamp/v7/routes/campgrounds.js
@@ -0,0 +1,75 @@
+var express = require("express");
+var router = express.Router(); //used for exporting the routes
+var Campground = require("../models/campground");
+
+//index campground
+router.get("/",function(req,res)
+{
+    //get all campgrounds from db
+    //req.user gives current user
+    
+    Campground.find({},function(err,allCampgrounds)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("campgrounds/index",{data: allCampgrounds,currentUser: req.user});
+        }
+    });
+    // res.render("campgrounds",{data: campgrounds});
+});
+
+//making a new campground
+router.post("/", function(req,res)
+{
+   //get data from from and add to campfround array
+   var name=req.body.name;
+   var image=req.body.image;
+   var dsc=req.body.description;
+   var val={name: name,image: image,description: dsc};
+   //create a new campground and save to a database 
+   Campground.create(val,function(err,nval)
+   {
+       if(err)
+       {
+           console.log(err);
+       }
+       else
+       {
+           //redirect back to campground
+           res.redirect("/campgrounds"); //default is a get request when redirecting so get method will run 
+       }
+   });
+});
+
+//a form to add new forms 
+router.get("/new",function(req, res) 
+{
+    res.render("campgrounds/new");
+});
+
+//order is important
+//shows more info about that campground
+router.get("/:id",function(req, res) 
+{
+    //find the campground with id
+    var id=req.params.id;
+    //we use the populate function for this 
+    Campground.findById(req.params.id).populate("comments").exec(function(err,val)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("campgrounds/show",{val: val});
+        }
+    });
+    //show template
+});
+
+module.exports = router;
---
+++ Node Practice/YelpCamp/v7/routes/comments.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var router = express.Router({mergeParams: true}); //for sending the id to the cooments
+var Campground = require("../models/campground"),
+    Comment = require("../models/comment");
+    
+//new comments
+router.get("/comments/new",isLoggedIn,function(req,res)
+{
+    Campground.findById(req.params.id,function(err,val)
+    {
+       if (err)
+       {
+           console.log(err);
+       }
+       else
+       {
+               res.render("comments/new",{val: val});
+       }
+    });
+});
+
+//when comments come here 
+router.post("/comments",isLoggedIn,function(req,res)
+{
+    //lookup for campground
+    Campground.findById(req.params.id,function(err, val) 
+    {
+        if(err)
+        {
+            res.redirect("/campgrounds");
+        }
+        else
+        {
+            //create a new comment
+            Comment.create(req.body.comments,function(err1,val1)
+            {
+                if(err1)
+                {
+                    console.log(err1);
+                }
+                else
+                {
+                    val.comments.push(val1);
+                    val.save();
+                    res.redirect("/campgrounds/" + val._id);
+                }
+            });
+        }
+    });
+});
+
+function isLoggedIn(req,res,next)
+{
+    if(req.isAuthenticated()) //checks if user is authenticated or not 
+    {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
